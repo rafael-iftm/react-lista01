e42edb4aa9d617fb6226c5bb651a38e68e890011
rafael-devbot <142061101+rafael-iftm@users.noreply.github.com>
2024-07-11 20:54:25 -0300

    feat: lista 05

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import React from 'react';
+import Header from './components/Header';
+import Content from './components/Content';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// src/components/Header.jsx
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>IFTM</h1>
+    </header>
+  );
+}
+
+export default Header;
